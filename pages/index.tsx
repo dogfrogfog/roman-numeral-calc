18cb3e1bfc870344efb72257906083c89b25ae39
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2023-02-15 17:42:58 +0100

    add basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,44 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 import styles from '@/pages/index.module.css'
+
+const MIN_INT_VALUE = 1
+const MAX_INT_VALUE = 1000;
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>roman-numeral-calc</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className={styles.mainSection}>
+        <InputSection />
+        <OutputSection />
       </main>
+    </div>
+  )
+}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+const InputSection = () => {
+  return (
+    <div className={styles.inputSection}>
+      <h3 className={styles.title}>Numeric input</h3>
+      <input
+        type="number"
+        min={MIN_INT_VALUE}
+        max={MAX_INT_VALUE}
+        className={styles.input}
+      />
+    </div>
+  )
+}
+
+const OutputSection = () => {
+  return (
+    <div className={styles.outputSection}>
+      <h3 className={styles.title}>Roman output</h3>
+      <p className={styles.outputValue}>XXXX||||</p>
     </div>
   )
 }
